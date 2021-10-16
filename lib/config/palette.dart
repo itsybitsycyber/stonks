a6062f214a782677b0addcd266f91414724192db
21560544 <1172-21560544@users.noreply.git.cs.sun.ac.za>
2021-10-16 13:06:36 +0200

    Add dependencies and colour palette.

---
+++ lib/config/palette.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Palette {
+  static const Color primaryColor = Color(0xFF8811); // Orange
+  static const Color secondaryColor = Color(0xFF8811); // Yellow
+  static const Color white = Color(0xFFF8F0); // White
+  static const Color blue = Color(0x9DD9D2); // Blue
+  static const Color darkBlue = Color(0x392F5A); // Dark Blue
+}
